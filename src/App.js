9c80f4417a948957e00b7841f0c4531e01d62850
Nicholas H <NicholasH7@hotmail.com>
2022-04-19 16:34:30 -0400

    Positioning KeyBoard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import './index.js'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+<div className='keyboard-input'>
+<textarea class="use-keyboard-input"/>
+    
+        <div className='keyboard-base'>
+        <div className = 'keyBoard-wrapper'>
+        <div className = 'keyNumbers'>
+
+        <button > 1 </button> 
+        <button > 2 </button> 
+        <button > 3 </button> 
+        <button > 4 </button> 
+        <button > 5 </button> 
+        <button > 6 </button> 
+        <button > 7 </button> 
+        <button > 8 </button> 
+        <button > 9 </button> 
+        <button > 0 </button> 
+        <button > - </button> 
+        <button > + </button> 
+        <button > = </button> 
+        <button className = 'span-two' > backspace </button>
+
+
+        
+        <button className = 'spantwo'> tab </button> 
+        <button > Q </button> 
+        <button > W </button> 
+        <button > E </button> 
+        <button > R </button> 
+        <button > T </button> 
+        <button > Y </button> 
+        <button > U </button> 
+        <button > I </button> 
+        <button > O </button> 
+        <button > P </button> 
+        <button > [ </button> 
+        <button> ] </button> 
+        <button className='span-two'> \ </button>
+
+
+        
+        <button className = 'span-two' > caps lock </button> 
+        <button> A </button> 
+        <button> S </button> 
+        <button> D </button> 
+        <button> G </button> 
+        <button> J </button> 
+        <button> K </button> 
+        <button> L </button> 
+        <button> ; </button> 
+        <button> ' </button> 
+        <button className = 'span-four'> enter </button>
+
+        <button className = 'span-two'> shift </button>
+        <button> Z </button> 
+        <button> X </button> 
+        <button> C </button> 
+        <button> V </button> 
+        <button> B </button> 
+        <button> N </button> 
+        <button> M </button> 
+        <button> , </button> 
+        <button> . </button>  
+        <button> / </button>
+       
+
+
+        <button className = 'span-two'> .com </button> 
+        <button> @ </button> 
+        <button className = 'span-twelve'> space bar </button>
+
+
+
+        </div> </div></div></div>
+    );
 }
 
 export default App;
